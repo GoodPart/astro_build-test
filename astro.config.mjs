5a36028de02d2b3d88893ee5d86a702b54711c70
GoodPart <pks940122@gmail.com>
2025-02-18 16:49:59 +0900

    임시 푸시

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,12 @@ import { defineConfig } from "astro/config";
 
 import htmlBeautifier from "astro-html-beautifier";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
+  // base: "/astro_build-test",
+  output: "static",
   integrations: [htmlBeautifier()],
+  adapter: vercel(),
 });
